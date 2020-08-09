648be0e847f73277b92f19ab90f2370f7fb91852
1-4200 <tkt193yk@gmail.com>
2020-08-09 20:30:01 +0900

    add: implements SolnSquareVerifier contract

--- eth-contracts/contracts/SolnSquareVerifier.sol
+++ eth-contracts/contracts/SolnSquareVerifier.sol
@@ -1,34 +1,45 @@
 pragma solidity >=0.4.21 <0.6.0;
 
-// TODO define a contract call to the zokrates generated solidity contract <Verifier> or <renamedVerifier>
+import "./Verifier.sol";
+import "./ERC721Mintable.sol";
 
+contract SolnSquareVerifier is ERC721MintableComplete {
+    using SafeMath for uint256;
 
+    Verifier public verifier;
 
-// TODO define another contract named SolnSquareVerifier that inherits from your ERC721Mintable class
+    struct solution {
+        uint256 index;
+        address sender;
+    }
 
+    solution[] private solutions;
+    mapping(bytes32 => solution) private submittedSolutions;
+    uint256 indexCounter = 0;
 
+    event SolutionAdded(bytes32 key, uint256 index, address sender);
 
-// TODO define a solutions struct that can hold an index & an address
+    constructor(address _verifierAddress, string memory _name, string memory _symbol) ERC721MintableComplete(_name, _symbol) public {
+        verifier = Verifier(_verifierAddress);
+    }
 
+    function addSolution(uint[2] memory _a, uint[2][2] memory _b, uint[2] memory _c, uint[2] memory _input) public {
+        bytes32 key = keccak256(abi.encodePacked(_a, _b, _c, _input));
+        require(submittedSolutions[key].index == 0, "require unique solution");
+        require(submittedSolutions[key].sender != address(0), "require unique solution");
+        solution memory newSolution = solution({index : indexCounter, sender : msg.sender});
+        submittedSolutions[key] = newSolution;
+        solutions.push(newSolution);
+        emit SolutionAdded(key, indexCounter, msg.sender);
+        indexCounter.add(1);
+    }
 
-// TODO define an array of the above struct
-
-
-// TODO define a mapping to store unique solutions submitted
-
-
-
-// TODO Create an event to emit when a solution is added
-
-
-
-// TODO Create a function to add the solutions to the array and emit the event
-
-
-
-// TODO Create a function to mint new NFT only after the solution has been verified
-//  - make sure the solution is unique (has not been used before)
-//  - make sure you handle metadata as well as tokenSuplly
+    function mintNFT(address _to, uint256 _tokenId, uint[2] memory _a, uint[2][2] memory _b, uint[2] memory _c, uint[2] memory _input) public {
+        require(verifier.verifyTx(_a, _b, _c, _input), "not verified");
+        addSolution(_a, _b, _c, _input);
+        super._mint(_to, _tokenId);
+    }
+}
 
   
 
